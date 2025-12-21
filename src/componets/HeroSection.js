9c2e5fccbdccf2f5a667d5c9d6042f6b82809845
ManikandanS2005 <geethamanimurugan2005@gmail.com>
2025-12-21 21:14:19 +0530

    fix :name isssue resolved

--- src/componets/HeroSection.js
+++ src/componets/HeroSection.js
@@ -9,10 +9,10 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen m-1 bg-gray-900 text-white text-center px-4">
       <h1 className="font-bold text-4xl mb-4">
-        Welcome to SmartAssess – AI-Powered MCQ Generator for Smarter Testing.
+      Welcome to Smart MCQ Plugin
       </h1>
       <span className="text-3xl text-gray-300">
-        Upload. Generate. Assess.
+        AI-Powered MCQ Generator for Smarter Testing
       </span>
       <button
         className="bg-blue-900 hover:bg-blue-700 hover:cursor-pointer px-4 py-1 m-2 rounded text-white font-semibold text-xl"
